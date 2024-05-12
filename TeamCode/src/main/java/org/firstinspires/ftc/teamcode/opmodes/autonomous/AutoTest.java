dc3579533f8738f22bbc779f508a49680453e218
Flo <fnlg@web.de>
2024-05-12 20:47:21 +0200

    SDK Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutoTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/*
+This is an automatically generated java file.
+Do_not_remove_this_string
+*/
+
+@Autonomous(name = "AutoTest")
+public class AutoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        opMode();
+    }
+
+    native void opMode();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/remotecontrolled/Guest1Driver.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes.remotecontrolled;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/*
+This is an automatically generated java file.
+Do_not_remove_this_string
+*/
+
+@TeleOp(name = "Infonachmittag 1 Driver", group = "Guest")
+public class Guest1Driver extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        opMode();
+    }
+    native void opMode();
+}
