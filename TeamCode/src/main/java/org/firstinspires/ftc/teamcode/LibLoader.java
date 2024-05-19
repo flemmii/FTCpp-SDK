a1e0ff275d2f09f154cfcef4accab38f8a497643
Flo <fnlg@web.de>
2024-05-19 20:37:11 +0200

    updated LibLoader

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LibLoader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LibLoader.java
@@ -6,29 +6,28 @@ import com.qualcomm.robotcore.eventloop.opmode.AnnotatedOpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
 
 
-public class LibLoader
-{
+public class LibLoader {
     private static boolean alreadyLoaded = false;
+
     /*
      * By annotating this method with @OpModeRegistrar, it will be called
      * automatically by the SDK as it is scanning all the classes in the app
      * (for @Teleop, etc.) while it is "starting" the robot.
      */
     @OpModeRegistrar
-    public static void loadNativeLibOnStartRobot(Context context, AnnotatedOpModeManager manager)
-    {
+    public static void loadNativeLibOnStartRobot(Context context, AnnotatedOpModeManager manager) {
         /*
          * Because this is called every time the robot is "restarted" we
          * check to see whether we've already previously done our job here.
          */
-        if(alreadyLoaded)
-        {
+        if (alreadyLoaded) {
             /*
              * Get out of dodge
              */
             return;
         }
 
-        System.loadLibrary("native-lib");
+        System.loadLibrary("sdk");
+        System.loadLibrary("teamcode");
     }
 }
