2a06f4ed22a8e09250fe50061926a09cd1492655
Flo <fnlg@web.de>
2024-05-22 22:15:32 +0200

    renamed rear motors

--- TeamCode/src/main/cpp/teamcode/sources/example.cpp
+++ TeamCode/src/main/cpp/teamcode/sources/example.cpp
@@ -18,8 +18,8 @@ Java_org_firstinspires_ftc_teamcode_opmodes_teleop_Example_opMode(JNIEnv *env,
     // This gets all the drive motors from the hardware map
     Dc_motor_ex front_right = hardware_map::get(DcMotorEx, "front_right");
     Dc_motor_ex front_left = hardware_map::get(DcMotorEx, "front_left");
-    Dc_motor_ex back_right = hardware_map::get(DcMotorEx, "back_right");
-    Dc_motor_ex back_left = hardware_map::get(DcMotorEx, "back_left");
+    Dc_motor_ex rear_right = hardware_map::get(DcMotorEx, "rear_right");
+    Dc_motor_ex rear_left = hardware_map::get(DcMotorEx, "rear_left");
 
     telemetry::add_line("Initialized");
     telemetry::update();
@@ -38,7 +38,7 @@ Java_org_firstinspires_ftc_teamcode_opmodes_teleop_Example_opMode(JNIEnv *env,
         // This sets the power of the motors to the y value of the left stick
         front_right.set_power(left_stick_y - left_stick_x - right_stick_x);
         front_left.set_power(left_stick_y + left_stick_x + right_stick_x);
-        back_right.set_power(left_stick_y + left_stick_x - right_stick_x);
-        back_left.set_power(left_stick_y - left_stick_x + right_stick_x);
+        rear_right.set_power(left_stick_y + left_stick_x - right_stick_x);
+        rear_left.set_power(left_stick_y - left_stick_x + right_stick_x);
     }
 }
